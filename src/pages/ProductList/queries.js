2af7f3c11976f95195743e19a6a83a80a2e6237b
Chukwuma Ezumezu <chukwuma.ezumezu@gmail.com>
2020-03-27 16:49:56 +0100

    refactor get products queries

---
+++ src/pages/ProductList/queries.js
@@ -0,0 +1,25 @@
+import gql from 'graphql-tag';
+import { PRODUCT_FRAGMENT } from '../../shared/queries';
+
+export const GET_CAROUSELS = gql`
+  {
+    carousels {
+      id
+      title
+      description
+      imageUrl
+    }
+  }
+`;
+
+export const GET_PRODUCTS = gql`
+  query Products($pageNumber: Float!) {
+    products(pageNumber: $pageNumber) {
+      products {
+        ...cardProduct
+      }
+      total
+    }
+  }
+  ${PRODUCT_FRAGMENT}
+`;
