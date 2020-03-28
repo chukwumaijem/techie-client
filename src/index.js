d30556fe9f995d1469a1a27a24d33450e1101362
Chukwuma Ezumezu <chukwuma.ezumezu@gmail.com>
2020-03-28 03:34:04 +0100

    refactor apollo setup

--- src/index.js
+++ src/index.js
@@ -1,35 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import { ApolloClient } from 'apollo-client';
 import { ApolloProvider } from '@apollo/react-hooks';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-import { HttpLink } from 'apollo-link-http';
-import { onError } from 'apollo-link-error';
-import { ApolloLink } from 'apollo-link';
 
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
-const link = ApolloLink.from([
-  new HttpLink({
-    uri: process.env.REACT_APP_SERVER_PATH
-  }),
-  onError(({ graphQLErrors, networkError }) => {
-    if (graphQLErrors)
-      graphQLErrors.forEach(({ message, locations, path }) =>
-        console.log(
-          `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
-        )
-      );
-    if (networkError) console.log(`[Network error]: ${networkError}`);
-  })
-]);
-
-const client = new ApolloClient({
-  link,
-  cache: new InMemoryCache()
-});
+import client from './store/apolloSetup';
 
 ReactDOM.render(
   <BrowserRouter>
